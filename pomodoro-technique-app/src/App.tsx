4f5d1bde5007c46d0344d44d7fc46e44e1fa7106
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-12-28 10:21:38 +0100

    function: create a function of numbers

--- pomodoro-technique-app/src/App.tsx
+++ pomodoro-technique-app/src/App.tsx
@@ -1,13 +1,40 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [days, setDays] = useState<number>(0);
+  const [hours, setHours] = useState<number>(0);
+  const [minutes, setMinutes] = useState<number>(0);
+  const [seconds, setSeconds] = useState<number>(0);
+
+  useEffect(() => {
+    const target = new Date("12/31/2023 23:59:59");
+
+    const interval = setInterval(() => {
+      const now = new Date();
+      const difference = target.getTime() - now.getTime();
+      const d = Math.floor((difference / 1000) * 60 * 60 * 24);
+      const h = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+        setHours(h)
+        const m = Math.floor(
+          (difference % (1000 * 60 * 60 * 24)) / (1000 * 60)
+        )
+        setMinutes(m)
+        const s = Math.floor((difference % (1000 * 60)) / 1000)
+        setSeconds(s)
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-          Hello World
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">Hello World Days{days}</header>
+      </div>
+    </>
   );
 }
 
