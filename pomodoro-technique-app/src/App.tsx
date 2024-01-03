f35f93d9d2fe13c44b2e1ffb3228501e9a31abb3
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2024-01-03 21:32:35 +0100

    fixing code

--- pomodoro-technique-app/src/App.tsx
+++ pomodoro-technique-app/src/App.tsx
@@ -2,28 +2,33 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [partyTime, setPartyTime] = useState(false);
   const [days, setDays] = useState<number>(0);
   const [hours, setHours] = useState<number>(0);
   const [minutes, setMinutes] = useState<number>(0);
   const [seconds, setSeconds] = useState<number>(0);
 
   useEffect(() => {
-    const target = new Date("12/31/2023 23:59:59");
+    const target = new Date("11/11/2024 23:59:59");
 
     const interval = setInterval(() => {
       const now = new Date();
       const difference = target.getTime() - now.getTime();
-      const d = Math.floor((difference / 1000) * 60 * 60 * 24);
+      const d = Math.floor(difference / (1000 * 60 * 60 * 24));
+      setDays(d);
       const h = Math.floor(
         (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
       );
-        setHours(h)
-        const m = Math.floor(
-          (difference % (1000 * 60 * 60 * 24)) / (1000 * 60)
-        )
-        setMinutes(m)
-        const s = Math.floor((difference % (1000 * 60)) / 1000)
-        setSeconds(s)
+      setHours(h);
+      const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      setMinutes(m);
+      const s = Math.floor((difference % (1000 * 60)) / 1000);
+      setSeconds(s);
+
+      if (d <= 0 && h <= 0 && m <= 0 && s <= 0) {
+        setPartyTime(true);
+        clearInterval(interval);
+      }
     }, 1000);
 
     return () => clearInterval(interval);
@@ -32,27 +37,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <div className="timer-inner">
-        <div className="timer-segment">
-          <span className="time">{days}</span>
-          <span className="label">Days</span>
+      {partyTime ? (
+        <h1>Happy NYE</h1>
+      ) : (
+        <div className="timer-inner">
+          <div className="timer-segment">
+            <span className="time">{days}</span>
+            <span className="label">Days</span>
+          </div>
+          <div className="timer-segment">
+            <span className="time">{hours}</span>
+            <span className="label">Hours</span>
+          </div>
+          <div className="timer-segment">
+            <span className="time">{minutes}</span>
+            <span className="label">Minutes</span>
+          </div>
+          <div className="timer-segment">
+            <span className="time">{seconds}</span>
+            <span className="label">Seconds</span>
+          </div>
         </div>
-        <div className="timer-segment">
-          <span className="time">{hours}</span>
-          <span className="label">Hours</span>
-        </div>
-        <div className="timer-segment">
-          <span className="time">{minutes}</span>
-          <span className="label">Minutes</span>
-        </div>
-        <div className="timer-segment">
-          <span className="time">{seconds}</span>
-          <span className="label">Seconds</span>
-        </div>
-      </div>
+      )}
     </div>
   );
-  
 }
 
 export default App;
