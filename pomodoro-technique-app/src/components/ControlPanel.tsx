f0329fd577e260a65cd66100871de5f73f7ef8eb
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2024-01-04 12:29:41 +0100

    structure

---
+++ pomodoro-technique-app/src/components/ControlPanel.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react'
+
+export const ControlPanel = () => {
+    return(
+        <div></div>
+    )
+}
+
+export default ControlPanel;
---
+++ pomodoro-technique-app/src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react'
+
+export const Header = () => {
+    return(
+        <div></div>
+    )
+}
+
+export default Header;
